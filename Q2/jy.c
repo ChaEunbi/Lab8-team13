4881b6edec7b8293f7d945dbde8daba0f0a27c75
ChaEunbi <21600705@handong.edu>
2017-05-31 20:30:17 +0900

    delete waitpid(보류), else

--- Q2/jy.c
+++ Q2/jy.c
@@ -4,31 +4,12 @@
 
 int main(int argc, char **argv, char **envp)
 {
-        int i=0;
-	int status,pid[argc],retpid;
+        int i=1;
+	int pid[argc];
 	
-//	for(i=0;i<argc;i++)
-		if((pid[i++]=fork())==0)
-		{
-			if((pid[i++]=fork())==0)
-			{
-				execve("/bin/gunzip",argv,NULL);
-			}
-			else{
-				execve("/bin/gunzip",argv,NULL);
-			}
-		}
-		else
-		{
-				execve("/bin/gunzip",argv,NULL);
-		}
-		
-	
-	i=0;
-        while((retpid = waitpid(pid[i++], &status, 0))> 0){
-                if(WIFEXITED(status))  
-                        printf("child %d terminated normally with exit status=%d\n",retpid,WEXITSTATUS(status));
-                else
-                        printf("child %d terminated abnormally\n", retpid);
-        }
+	if((pid[i++]=fork())==0)
+	{
+		execve("/bin/gzip",argv,NULL);
+		exit(0);
+	}
 }
